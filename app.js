117e80ccf737f242533f259e7d9f4bc8e9f3bc1b
lauraFortune <48602973+lauraFortune@users.noreply.github.com>
2024-10-05 19:57:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,12 +23,8 @@ app.use(helmet({
                 "'self'",
                 "https://ajax.googleapis.com", // jQuery CDN,
                 "https://static.cloudflareinsights.com",
+                "https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015",
                 "'unsafe-inline'"
-            ],
-            scriptSrcElem: [
-                "'self'",
-                "https://ajax.googleapis.com", // jQuery CDN,
-                "https://static.cloudflareinsights.com",
             ],
             styleSrc: [
                 "'self'",
